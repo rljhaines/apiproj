eb99035f6fd7ab41e9e41e0e695c27722cfd0030
rljhaines <rljhaines@gmail.com>
2015-02-03 21:11:32 -0600

    add simpfunc to api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def simpfunc(request):
+    return HttpResponse('Why did we call our App "api?"')
 
 # Create your views here.
