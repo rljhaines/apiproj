a7d064f7e1b71ce2462b3e3ba9bed57598e801b0
rljhaines <rljhaines@gmail.com>
2015-02-04 18:22:18 -0600

    added another function and url

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from api import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.simpfunc),
+        url(r'simpfunc/', views.simpfunc),
+        url(r'otherfunc/', views.anotherfunc),
 )
--- api/views.py
+++ api/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 def simpfunc(request):
     return HttpResponse('Why did we call our App "api?"')
 
+def anotherfunc(request):
+    #print 'hello'
+    return HttpResponse('<h2>Is HttpResponse required for a url view?</h2>')
+
+#def anotherfunc(request):
+    #print 'this is using a print function'
+
 # Create your views here.
